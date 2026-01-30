56df9cc0ed693ae6aa285b927564ed7d0438d7e2
sakib700199-commits <sakib700199@gmail.com>
2026-01-31 01:44:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "")
+BOT_TOKEN = getenv("7761547492:AAF8ubskMrS1fvuTBb0dO_9w_c-X8y0zmLw", "")
+BOT_USERNAME = getenv("http://t.me/RUHI_X_QNR_GNG_BOT", "")
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("29568441"))
+API_HASH = getenv("b32ec0fb66d22da6f77d355fbace4f2a", "")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
